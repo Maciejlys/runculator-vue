e62122ce745a7cafd86134dde2e2a183149a6a8b
maciej.lyszczarz <maciej.lyszczarz@jamf.com>
2024-01-19 21:00:47 +0100

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+</script>
+
+<template>
+  <main>
+    <Button>Shadcn</Button>
+    <Input />
+  </main>
+</template>
